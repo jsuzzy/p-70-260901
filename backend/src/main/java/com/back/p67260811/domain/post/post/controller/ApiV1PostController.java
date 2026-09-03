591496c899a00cec9f713e57734f79d55bfb9090
jeongsuji <sujijeong11@gmail.com>
2026-09-03 09:28:51 +0900

    권한 검증 로직은 엔티티로 이동

--- backend/src/main/java/com/back/p67260811/domain/post/post/controller/ApiV1PostController.java
+++ backend/src/main/java/com/back/p67260811/domain/post/post/controller/ApiV1PostController.java
@@ -97,9 +97,7 @@ public class ApiV1PostController {
         Member actor = rq.getActor();
         Post post = postService.findById(id).get();
 
-        if(!actor.equals(post.getAuthor())){
-            throw new ServiceException("403-1", "수정 권한이 없습니다.");
-        }
+        post.checkActorModify(actor);
 
         postService.modify(post, reqBody.title, reqBody.content);
 
@@ -117,8 +115,7 @@ public class ApiV1PostController {
         Member actor = rq.getActor();
         Post post = postService.findById(id).get();
 
-        if(!actor.equals(post.getAuthor())) throw new ServiceException("403-1", "삭제 권한이 없습니다.");
-
+        post.checkActorDelete(actor);
 
         postService.delete(id);
 
