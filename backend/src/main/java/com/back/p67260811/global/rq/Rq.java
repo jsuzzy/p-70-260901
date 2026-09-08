77e92f0d9fbdfabf0ed919384dc1c83a3b20f134
jeongsuji <sujijeong11@gmail.com>
2026-09-08 12:38:53 +0900

    CustomAuthenticationFilter에 인증 로직 추가

--- backend/src/main/java/com/back/p67260811/global/rq/Rq.java
+++ backend/src/main/java/com/back/p67260811/global/rq/Rq.java
@@ -82,11 +82,11 @@ public class Rq {
 
     }
 
-    private void setHeader(String name, String value) {
+    public void setHeader(String name, String value) {
         response.setHeader(name, value);
     }
 
-    private String getCookieValue(String name, String defaultValue) {
+    public String getCookieValue(String name, String defaultValue) {
         return Optional
                 .ofNullable(request.getCookies())
                 .flatMap(
@@ -121,7 +121,7 @@ public class Rq {
 
     }
 
-    private String getHeader(String name, String defaultValue) {
+    public String getHeader(String name, String defaultValue) {
         return Optional
                 .ofNullable(request.getHeader(name))
                 .filter(headerValue -> !headerValue.isBlank())
