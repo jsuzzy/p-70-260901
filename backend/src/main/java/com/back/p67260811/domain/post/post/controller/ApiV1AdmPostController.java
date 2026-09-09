689ae0d2299615e817c9ba2a8ca28c25e5934345
jeongsuji <sujijeong11@gmail.com>
2026-09-09 09:36:25 +0900

    인증 없이 가능한 요청을 securityConfig에 등록

--- backend/src/main/java/com/back/p67260811/domain/post/post/controller/ApiV1AdmPostController.java
+++ backend/src/main/java/com/back/p67260811/domain/post/post/controller/ApiV1AdmPostController.java
@@ -33,14 +33,6 @@ public class ApiV1AdmPostController {
     @Operation(summary = "글 개수 조회")
     public CountResBody count() {
 
-        //1. 인증
-        Member actor = rq.getActor();
-
-        //2. 인가
-        if(!actor.isAdmin()) {
-            throw new ServiceException("403-1", "권한이 없습니다");
-        }
-
         return new CountResBody(postService.count());
     }
 
--- backend/src/main/java/com/back/p67260811/global/security/SecurityConfig.java
+++ backend/src/main/java/com/back/p67260811/global/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.back.p67260811.global.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,8 +25,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers("/favicon.ico").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers("/**").permitAll()
-                        .anyRequest().authenticated())
+                        .requestMatchers(HttpMethod.GET, "/api/*/posts", "/api/*/posts/{id:\\d+}",
+                                "/api/*/posts/{postId:\\d+}/comments", "/api/*/posts/{postId:\\d+}/comments/{commentId:\\d+}").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/members/login", "/api/v1/members/join").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/members/logout").permitAll()
+                        .requestMatchers("/api/*/**").authenticated())
                 .csrf((csrf) -> csrf.disable())
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
